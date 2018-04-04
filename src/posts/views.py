7d83838ae7430a35567d85a8957d7851525e1c9c
saviolinski <saviolinski@gmail.com>
2018-04-04 17:18:19 +0200

    search bar + back pagination

--- src/posts/views.py
+++ src/posts/views.py
@@ -5,6 +5,8 @@ from django.db.models import Q
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 from .models import Post
 from .forms import PostForm
@@ -43,11 +45,26 @@ def posts_list(request):
 	return render(request, "post_list.html", context)
 
 def index(request):
-	queryset = Post.objects.all()[:3] #.order_by("-timestamp")
+	queryset_list = Post.objects.all() #.order_by("-timestamp")
+
+	paginator = Paginator(queryset_list, 3)
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
 	context = {
-		"object_list": queryset
+		"object_list": queryset,
+		"page_request_var": page_request_var
 	}
 	return render(request, "index.html", context)
+
 
 def posts_update(request, slug):
 	instance = get_object_or_404(Post, slug=slug)
